01365f0037908a488dd303596d157592a614d4ce
Joseph Cosentino <joecose@amazon.com>
2022-03-23 23:57:29 -0400

    chore: unused import

--- src/main/java/com/aws/greengrass/mqttbridge/MQTTBridge.java
+++ src/main/java/com/aws/greengrass/mqttbridge/MQTTBridge.java
@@ -33,7 +33,6 @@ import lombok.Getter;
 import java.io.IOException;
 import java.security.KeyStoreException;
 import java.security.cert.CertificateException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
