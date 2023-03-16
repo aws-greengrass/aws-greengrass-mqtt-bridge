397e1c0a7c1e208999f6de42537e59a3b9a3b5f0
Joseph Cosentino <joecose@amazon.com>
2023-03-15 17:02:43 -0700

    feat: mqtt5 integration test

---
+++ src/integrationtests/java/org/junit/runners/model/Statement.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.junit.runners.model;
+
+// testcontainers still has some dependencies on junit4.
+// we exclude junit4 from testcontainers in pom.xml, but
+// it still looks for this class at runtime.
+// once testcontainers is version 2.x, we can remove this.
+public class Statement {
+}
