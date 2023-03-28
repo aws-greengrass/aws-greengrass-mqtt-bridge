ee05c77ab95deb70eda942aff72cc5eb16ce2f4d
Joseph Cosentino <joecose@amazon.com>
2023-03-28 13:56:11 -0700

    fix: unsubscribe from keystore updates

--- src/test/java/com/aws/greengrass/mqtt/bridge/clients/MQTTClientTest.java
+++ src/test/java/com/aws/greengrass/mqtt/bridge/clients/MQTTClientTest.java
@@ -31,7 +31,9 @@ import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -84,6 +86,7 @@ public class MQTTClientTest {
 
         mqttClient.stop();
 
+        verify(mockMqttClientKeyStore).unsubscribeFromCAUpdates(any());
         subscriptions = fakeMqttClient.getSubscriptionTopics();
         assertThat(fakeMqttClient.isConnected(), is(false));
         assertThat(subscriptions, hasSize(0));
