02dace4e4421757d3e754b3a0b40a5483ab6761b
Joseph Cosentino <joecose@amazon.com>
2023-03-29 18:50:52 -0700

    chore: don't copy list

--- src/main/java/com/aws/greengrass/mqtt/bridge/auth/MQTTClientKeyStore.java
+++ src/main/java/com/aws/greengrass/mqtt/bridge/auth/MQTTClientKeyStore.java
@@ -140,7 +140,7 @@ public class MQTTClientKeyStore {
     }
 
     private synchronized List<String> getCaCerts() {
-        return new ArrayList<>(caCerts);
+        return caCerts;
     }
 
     /**
